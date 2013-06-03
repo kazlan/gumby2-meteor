cf826bafe5f790396de652c63c944fcefd357c6e
redbaron76 <f.fumis@gmail.com>
2013-06-03 14:30:34 +0200

    Created Gumby Css Package

--- package.js
+++ package.js
@@ -6,6 +6,10 @@ Package.on_use(function (api, where) {
 
 	api.use(['templating'], 'client');
 
+	api.add_files('fonts/icons/entypo.eot', 'client');
+	api.add_files('fonts/icons/entypo.ttf', 'client');
+	api.add_files('fonts/icons/entypo.woff', 'client');
+
 	api.add_files([
 		'css/gumby.css',
 		'js/libs/modernizr-2.6.2.min.js',
