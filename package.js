41c69200c61f735d1439ea6f6d1ebfc223a3a414
Kelly Copley <copleykj@gmail.com>
2013-08-20 00:41:12 -0400

    0.6.5 updates

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-	summary: "Gumby Css Framework v2 for Meteor"
+	summary: "Gumby Css Framework v2 for Meteor with Gumby Initialization and mobile checks"
 });
 
 Package.on_use(function (api, where) {
 
-	api.use(['templating'], 'client');
+	api.use(['jquery'], 'client');
 
 	api.add_files('fonts/icons/entypo.eot', 'client');
 	api.add_files('fonts/icons/entypo.ttf', 'client');
@@ -13,6 +13,10 @@ Package.on_use(function (api, where) {
 	api.add_files([
 		'css/gumby.css',
 		'js/libs/modernizr-2.6.2.min.js',
-		'js/libs/gumby.min.js',
+		'js/libs/jquery.mobile.custom.min.js',
+		'js/libs/gumby.js',
+		'js/main.js',
 		'js/plugins.js'], 'client');
+
+	api.export(['Modernizr', 'Gumby'], "client");
 });
